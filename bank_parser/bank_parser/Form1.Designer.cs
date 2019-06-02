b709d3a3a4e99526973e31daa9ce15b45eb62b60
Twuzcher <nik.tsatsura99@mail.ru>
2019-06-03 02:07:02 +0300

    Stage_1/Internet_connection

--- bank_parser/bank_parser/Form1.Designer.cs
+++ bank_parser/bank_parser/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(290, 123);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(188, 79);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Приветствую! \r\nДобро пожаловать \r\nв приложение!";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(290, 208);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(188, 79);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Выход";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(794, 441);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
     }
 }
 
--- bank_parser/bank_parser/Form1.cs
+++ bank_parser/bank_parser/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 namespace bank_parser
 {
@@ -15,6 +16,43 @@ namespace bank_parser
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (!CheckForInternetConnection())
+            {
+                MessageBox.Show("Отсутствует интернет соединение, пожалуйста подключитесь к интернету!", "Отсутствует интернет соединение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if(CheckForInternetConnection())
+            {
+                
+                MainForm mainForm = new MainForm();
+                Hide();
+                mainForm.ShowDialog();
+                Show();
+            }
+        }
+
+        public bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
